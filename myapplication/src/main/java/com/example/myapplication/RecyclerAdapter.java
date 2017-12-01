b3861c8e2412beee780f1ccad99a98d65be8ec91
chauzov_n_g <chauzov1995@yandex.ru>
2017-12-01 16:59:01 +0300

    sadasd

---
+++ myapplication/src/main/java/com/example/myapplication/RecyclerAdapter.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+
+        import android.support.v7.widget.RecyclerView;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import java.util.ArrayList;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+
+    ArrayList<Product> objects;
+
+    // класс view holder-а с помощью которого мы получаем ссылку на каждый элемент
+    // отдельного пункта списка
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // наш пункт состоит только из одного TextView
+        public TextView mTextView;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = (TextView) v.findViewById(R.id.tvText);
+        }
+    }
+
+    // Конструктор
+    public RecyclerAdapter(ArrayList<Product> _objects) {
+        objects = _objects;
+    }
+
+    // Создает новые views (вызывается layout manager-ом)
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_green, parent, false);
+
+        // тут можно программно менять атрибуты лэйаута (size, margins, paddings и др.)
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Заменяет контент отдельного view (вызывается layout manager-ом)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.mTextView.setText("asdasdasd");
+
+    }
+
+    // Возвращает размер данных (вызывается layout manager-ом)
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
--- myapplication/src/main/java/com/example/myapplication/fragment_dohod.java
+++ myapplication/src/main/java/com/example/myapplication/fragment_dohod.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,28 +43,32 @@ static boolean lonpower=false;
 
 
 
-        BoxAdapter  boxAdapter1 = new BoxAdapter(getActivity(), MainActivity.products_doh, getActivity());
-        BoxAdapter  boxAdapter2 = new BoxAdapter(getActivity(), MainActivity.products_rash, getActivity());
-        BoxAdapter  boxAdapter3 = new BoxAdapter(getActivity(), MainActivity.products_zel, getActivity());
+      //  BoxAdapter  boxAdapter1 = new BoxAdapter(getActivity(), MainActivity.products_doh, getActivity());
+        //BoxAdapter  boxAdapter2 = new BoxAdapter(getActivity(), MainActivity.products_rash, getActivity());
+        //BoxAdapter  boxAdapter3 = new BoxAdapter(getActivity(), MainActivity.products_zel, getActivity());
+      //  String[] myDataset = getActivity().getApplication().getDataSet();
+        RecyclerAdapter  boxAdapter1 = new RecyclerAdapter(MainActivity.products_zel);
+     //   RecyclerAdapter  boxAdapter2 = new RecyclerAdapter(getDataSet());
+    //    RecyclerAdapter  boxAdapter3 = new RecyclerAdapter(getDataSet());
         // настраиваем список
-        GridView gvdoh = (GridView) v.findViewById(R.id.gvdoh);
-        GridView gvrash = (GridView) v.findViewById(R.id.gvrash);
-        GridView gvzel = (GridView) v.findViewById(R.id.gvzel);
+      RecyclerView gvdoh = (RecyclerView) v.findViewById(R.id.gvdoh);
+       RecyclerView gvrash = (RecyclerView) v.findViewById(R.id.gvrash);
+       RecyclerView gvzel = (RecyclerView) v.findViewById(R.id.gvzel);
         ScrollView scrview = (ScrollView) v.findViewById(R.id.scrview);
 
 
 
 
-        final float scale = getActivity().getResources().getDisplayMetrics().density;
-        int size_gv_doh=(int)Math.ceil(((double)MainActivity.products_doh.size())/3);
-        int size_gv_rash= (int)Math.ceil(((double)MainActivity.products_rash.size())/3);
-        int size_gv_zel=(int)Math.ceil(((double)MainActivity.products_zel.size())/3);
-        int vsegoheight=(size_gv_zel+size_gv_doh+size_gv_rash)*134+4;
-        LinearLayout llheight = (LinearLayout) v.findViewById(R.id.llheight);
-        ViewGroup.LayoutParams params = llheight.getLayoutParams();
-        int pixels = (int) (vsegoheight * scale + 0.5f);
-        params.height = pixels; // или в пикселях
-        llheight.setLayoutParams(params);
+     //   final float scale = getActivity().getResources().getDisplayMetrics().density;
+     //   int size_gv_doh=(int)Math.ceil(((double)MainActivity.products_doh.size())/3);
+     //   int size_gv_rash= (int)Math.ceil(((double)MainActivity.products_rash.size())/3);
+     //   int size_gv_zel=(int)Math.ceil(((double)MainActivity.products_zel.size())/3);
+     //   int vsegoheight=(size_gv_zel+size_gv_doh+size_gv_rash)*134+4;
+     //   LinearLayout llheight = (LinearLayout) v.findViewById(R.id.llheight);
+     //   ViewGroup.LayoutParams params = llheight.getLayoutParams();
+     //   int pixels = (int) (vsegoheight * scale + 0.5f);
+     //   params.height = pixels; // или в пикселях
+    //    llheight.setLayoutParams(params);
 
         gvdoh.setOnLongClickListener(new View.OnLongClickListener() {
             public boolean onLongClick(View r) {
@@ -80,8 +85,8 @@ static boolean lonpower=false;
 
 
         gvdoh.setAdapter(boxAdapter1);
-        gvrash.setAdapter(boxAdapter2);
-        gvzel.setAdapter(boxAdapter3);
+     //   gvrash.setAdapter(boxAdapter2);
+      //  gvzel.setAdapter(boxAdapter3);
 
     }
 
